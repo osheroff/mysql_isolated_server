0329448bc7f4d72192d3d5c84e96175af35a0c06
Gabe Martin-Dempesy <gabe@zendesk.com>
2014-07-17 15:17:35 -0700

    add mysql make_slave_of specs

--- spec/lib/isolated_server/mysql_spec.rb
+++ spec/lib/isolated_server/mysql_spec.rb
@@ -23,11 +23,55 @@ describe IsolatedServer::Mysql do
     end
   end
 
+  describe "#make_slave_of" do
+    let(:master) { IsolatedServer::Mysql.new }
+    let(:slave)  { IsolatedServer::Mysql.new }
+    let(:db_name) { 'integration_test' }
+    let(:person_name) { 'Bob' }
+
+    it "sets establishes the relationship" do
+      master.boot!
+      slave.boot!
+
+      slave.make_slave_of(master)
+      sleep 0.1
+      expect(slave_status(slave)['Slave_IO_State']).to eq("Waiting for master to send event")
+
+      master.connection.query("CREATE DATABASE #{db_name}")
+      master.connection.select_db(db_name)
+      master.connection.query(create_table_sql)
+      master.connection.query("INSERT INTO people SET name = '#{person_name}'")
+      sleep 0.1
+
+      slave.connection.select_db(db_name)
+      slave_result = slave.connection.query('SELECT name FROM people').first
+      expect(slave_result['name']).to eq person_name
+
+      master.down!
+      slave.down!
+
+    end
+  end
+
   def query_mysql_global_variable(isolated_database, key)
     isolated_database.
       connection.
-      query("show global variables like '#{key}'").
+      query("SHOW GLOBAL VARIABLES LIKE '#{key}'").
       first["Value"]
   end
+
+  def slave_status(isolated_database)
+    isolated_database.connection.query('SHOW SLAVE STATUS').first
+  end
+
+    def create_table_sql
+      <<-eos
+CREATE TABLE `people` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+eos
+    end
 
 end
